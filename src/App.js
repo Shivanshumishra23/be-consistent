713cc6b3427b71ed753620929e1a019291a2851e
Shivanshu Mishra <mishrashivanshu2004@gmail.com>
2023-08-18 13:08:53 +0530

    This is my final version of Be consistent project

--- src/App.js
+++ src/App.js
@@ -1,40 +1,50 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import { Route , Routes } from "react-router-dom";
+import Navbar from './Component/Navbar';
+import Home from './Component/Home';
+import About from './Component/About';
+import Contact from './Component/Contact';
 
 
+
+
+
+    
 
 function App() {
-
-
-  async function getData(){
-  
- 
- 
-  
-    let response, data, runURL ;
-    try{
-      runURL= "https://parsehub.com/api/v2/runs/t8oYhTn83vDq/data?api_key=taCT_3Pp0mwO";
-      response = await fetch(runURL);
-      data = await response.json;
-    }
-    catch(err){ console.log(err);}
-    
-  
-    
-
-    
-    console.log(data);
- 
- }
-
-
-
   return (
-    <div className="App">
-     <button onClick={getData}> hello</button>
+    <div className='relative z-0 bg-primary gradient1 '>
+      
+      <div className='h-full bg-hero-pattern bg-center'>
+        <Navbar/>
+      </div>
+    
+       
+     
+        <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        {/* <Route path="/help" element={<Help />} /> */}
+      </Routes>
 
-    </div>
+    
+
+
+
+     
+
+
+
+
+
+      </div>
+ 
   );
 }
+
+
 
 export default App;
